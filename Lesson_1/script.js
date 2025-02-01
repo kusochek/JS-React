d8cf4ad5bc07911585c059975121d8bbb94334e8
kusochek <kusochek1995@gmail.com>
2025-02-01 12:21:03 +0100

    update lesson 1

--- Lesson_1/script.js
+++ Lesson_1/script.js
@@ -1,12 +1,48 @@
-console.log("Hello world!");
+// console.log("Hello world!");
 
 // STRING - "Hello world!" 'Hello! My name is Sasha. I'm 29 years old'
 // BOOLEAN - true/false
 // NUMBER - 0 1 2 1023
+// NULL
+// UNDEFINED
 
-console.log(false);
-console.log(10);
+// console.log(false);
+// console.log(10);
 
-console.log('I\'m 29 years old!');
-console.log(29);
+// console.log('I\'m 29 years old!');
+// console.log(29);
+
+// years = 29;
+// firstName = 'Sasha';
+// havePet = true;
+
+// console.log(years);
+// console.log(firstName);
+// console.log(havePet);
+
+// petya = 5;
+// vasya = 7;
+
+// totalNumberOfApple = petya + vasya;
+// total_number_of_apple - DON'T USE IT!!!
+
+// console.log(totalNumberOfApple);
+
+// firstName = 'Vladyslav';
+// lastName = 'Melnyk';
+
+// fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// alert('Error error error!!!');
+// years = prompt('How old are you?', 25);
+// console.log(years);
+
+// console.log('Hello! I\'m' + ' ' + years + ' ' + 'years old!');
+
+// console.log(29, 'Hello', 100, true);
+
+havePet = confirm('Do you have pet?');
+console.log(havePet);
+
 
