349ddee81e65a9f8361fbbc339b45d8d0b1e9604
kusochek <kusochek1995@gmail.com>
2025-02-01 11:18:45 +0100

    Lesson 1

---
+++ Lesson_1/script.js
@@ -0,0 +1,8 @@
+console.log("Hello world!");
+
+// STRING - "Hello world!" 'Hello! My name is Sasha. I'm 29 years old'
+// BOOLEAN - true/false
+// NUMBER - 0 1 2 1023
+
+console.log(false);
+console.log(10);
