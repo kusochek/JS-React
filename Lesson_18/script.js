5779e9b2f1edaa8573f0ebd4fe29d8c4e8214edd
kusochek <kusochek1995@gmail.com>
2025-03-19 19:11:53 +0100

    lesson_18

---
+++ Lesson_18/script.js
@@ -0,0 +1,51 @@
+// cut, copy, paste
+// const firstName = document.querySelector('#firstName');
+
+// firstName.addEventListener('copy', () => {
+//   console.log('copy');
+// });
+
+
+// firstName.addEventListener('paste', () => {
+//   console.log('paste');
+// });
+
+
+// firstName.addEventListener('cut', () => {
+//   console.log('cut');
+// });
+
+
+// Bubbling and capturing
+// stopPropagation
+
+// const span = document.querySelector('#span');
+// const p = document.querySelector('#p');
+// const div = document.querySelector('#div');
+// const anotherDiv = document.querySelector('.another-div');
+
+// span.addEventListener('click', (e) => {
+//   e.stopPropagation();
+//   console.log('span');
+// })
+
+// p.addEventListener('click', (e) => {
+//   e.stopPropagation();
+//   console.log('p');
+// })
+
+// div.addEventListener('click', () => {
+//   console.log('div');
+// })
+
+
+// anotherDiv.addEventListener('click', () => {
+//   console.log('anotherDiv');
+// })
+
+
+// localStorage/sessionStorage
+// setItem(key, value)
+// getItem(key)
+// removeItem(key)
+// JSON.parse/JSON.stringify
